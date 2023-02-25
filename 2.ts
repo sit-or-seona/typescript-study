0550fad63ecb07cad6191a1c57f0ef0375a1fde2
seona.bang <sit.or.seona@gmail.com>
2023-02-25 20:26:56 +0900

    solve: excercise 2

---
+++ 2.ts
@@ -0,0 +1,43 @@
+interface User {
+  name: string;
+  age: number;
+  occupation: string;
+}
+
+interface Admin {
+  name: string;
+  age: number;
+  role: string;
+}
+
+// 유니온 타입
+export type Person = User | Admin;
+
+export const persons: Person[] /* <- Person[] */ = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Jane Doe",
+    age: 32,
+    role: "Administrator",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+  {
+    name: "Bruce Willis",
+    age: 64,
+    role: "World saver",
+  },
+];
+
+export function logPerson(user: Person) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson);
