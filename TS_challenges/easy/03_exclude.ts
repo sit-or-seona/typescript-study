b191150b782df26fa4af34928ee8668fc6bbb50e
seona.bang <sit.or.seona@gmail.com>
2023-03-23 22:40:24 +0900

    solve: TS challenges easy 03_exclude

---
+++ TS_challenges/easy/03_exclude.ts
@@ -0,0 +1,45 @@
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
+
+  ### Question
+
+  Implement the built-in Exclude<T, U>
+
+  > Exclude from T those types that are assignable to U
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+
+/* 
+Utility Type - Exclude<Type1, Type2>
+유니온 타입인 <Type1>에서 <Type2>를 제외한 타입을 반환
+*/
+type MyExclude<T, U> = Exclude<T, U>;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a">, "b" | "c">>,
+  Expect<Equal<MyExclude<"a" | "b" | "c", "a" | "b">, "c">>,
+  Expect<
+    Equal<MyExclude<string | number | (() => void), Function>, string | number>
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
