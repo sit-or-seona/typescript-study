23bea578e0c1dfc07324bc4d626e86ad09008457
seona.bang <sit.or.seona@gmail.com>
2023-04-25 16:40:00 +0900

    solve: TS challenges medium 04_BEMstylestring

---
+++ TS_challenges/medium/04_BEMstylestring.ts
@@ -0,0 +1,59 @@
+/*
+  3326 - BEM style string
+  -------
+  by Songhn (@songhn233) #medium #template-literal #union #tuple
+
+  ### Question
+
+  The Block, Element, Modifier methodology (BEM) is a popular naming convention for classes in CSS.
+
+  For example, the block component would be represented as `btn`, element that depends upon the block would be represented as `btn__price`, modifier that changes the style of the block would be represented as `btn--big` or `btn__price--warning`.
+
+  Implement `BEM<B, E, M>` which generate string union from these three parameters. Where `B` is a string literal, `E` and `M` are string arrays (can be empty).
+
+  > View on GitHub: https://tsch.js.org/3326
+*/
+
+/* _____________ Your Code Here _____________ */
+
+/* 
+배열인 Element와 Modifier가 null인 경우를 처리하기 위해
+새로운 타입을 만들어 조건식으로 구분하여 처리
+*/
+type Element<E extends string[]> = E[number] extends null
+  ? ``
+  : `__${E[number]}`;
+type Modifier<M extends string[]> = M[number] extends null
+  ? ``
+  : `--${M[number]}`;
+type BEM<
+  B extends string,
+  E extends string[],
+  M extends string[]
+> = `${B}${Element<E>}${Modifier<M>}`;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from "@type-challenges/utils";
+
+type cases = [
+  Expect<Equal<BEM<"btn", ["price"], []>, "btn__price">>,
+  Expect<
+    Equal<
+      BEM<"btn", ["price"], ["warning", "success"]>,
+      "btn__price--warning" | "btn__price--success"
+    >
+  >,
+  Expect<
+    Equal<
+      BEM<"btn", [], ["small", "medium", "large"]>,
+      "btn--small" | "btn--medium" | "btn--large"
+    >
+  >
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3326/answer
+  > View solutions: https://tsch.js.org/3326/solutions
+  > More Challenges: https://tsch.js.org
+*/
